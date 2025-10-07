1c04843e33ec21f144ed5c57dd1009e598b53557
PJ <52793278+pieterjanvc@users.noreply.github.com>
2025-10-07 08:39:04 -0400

    Prompt (#2)
    * Add rubric an create DB
    
    * add link

--- dev/dev.R
+++ dev/dev.R
@@ -1,3 +1,51 @@
+library(dplyr)
+library(readxl)
+library(stringr)
+library(sqlife)
+# devtools::install_github("pieterjanvc/sqlife", ref = "data_manipulation")
+
+# Create a prompt to evaluate the reviews according to the rubric
+data <- read_xlsx("local/BIDMC_Med_Neuro_SPE_Comments_Dataset_07242025.xlsx", 1)
+colnames(data) <- str_replace_all(tolower(colnames(data)), "\\s+", "_")
+data <- data |>
+  mutate(
+    rowid = as.integer(rowid),
+    submission_date = as.character(submission_date)
+  )
+
+dataDict <- read_xlsx(
+  "local/BIDMC_Med_Neuro_SPE_Comments_Dataset_07242025.xlsx",
+  2
+) |>
+  mutate(COLUMN = str_replace_all(tolower(COLUMN), "\\s+", "_"))
+colnames(dataDict) <- str_replace_all(tolower(colnames(dataDict)), "\\s+", "_")
+
+dataDict <- dataDict |>
+  left_join(
+    data.frame(
+      column = colnames(data),
+      type = sapply(data[1, ], function(x) {
+        ifelse(
+          class(x)[1] == "numeric",
+          "INTEGER",
+          "TEXT"
+        )
+      }) |>
+        unlist()
+    ),
+    by = "column"
+  ) |>
+  select(column, type, definition)
+
+dataDict |> select(-definition) |> print(n = 50)
+
+sqlife::sql_create(data, "evals")
+
+result <- dbSetup("local/cfme.db", "inst/cfme.sql", validateSchema = T)
+result <- tbl_insert(data, "local/cfme.db", "evals")
+
 # Test
-resp <- llm_call("Write a limerick about a bird", log = "local/apiLog.csv")
+sysPrompt <- readLines("inst/rubricPrompt.txt") |> paste(collapse = "\n")
+prompt <- paste(sysPrompt, "#EVALUATION\n\n", data$answer_txt[1])
+resp <- llm_call(prompt, log = "local/apiLog.csv")
 resp$choices[[1]]$message$content |> cat()
