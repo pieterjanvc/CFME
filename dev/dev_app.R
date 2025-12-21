71fb6c961dfade76d9feeca0080efb391af7fed2
pieterjanvc <pieterjanvc@hotmail.com>
2025-12-21 08:55:17 -0500

    dev plumber integration exploration

--- dev/dev_app.R
+++ dev/dev_app.R
@@ -1,28 +1,11 @@
-ui <- fluidPage(
-  div("fheoifjeowi fhweifjewoi efwiwejqfo"),
-  actionButton("btn", "Click"),
-  tags$div(
-    id = "sel-box",
-    "oke with student doctor [name_redact] about next steps in her growth. We agreed to continue focusing on moving from mainly reporting the data/events, to synthesizing them into an assessment and actionable items for the day. Also encouraged her to re-organize problem lists as needed and not always stick to what is written in the overnight admission note. She bega"
-  ),
-  mod_highlight_ui("test", element = "sel-box")
-)
+library(plumber2)
 
-server <- function(input, output, session) {
-  selections <- reactiveVal(c("111", "222"))
-  test <- mod_highlight_server(
-    "test",
-    defaults = selections,
-    reset = reactive(input$btn)
+x <- api() |>
+  api_shiny(
+    "/ui/",
+    shiny::shinyAppDir(
+      system.file("examples-shiny", "01_hello", package = "shiny")
+    )
   )
 
-  # observeEvent(input$btn, {
-  #   selections(sample(1:100, sample(1:3, 1)) |> as.character())
-  # })
-
-  observe({
-    test()
-  })
-}
-
-shinyApp(ui, server)
+x |> api_run()
