392a5dbbdad29787b2a5845ff3976a9c61cbcfd3
pieterjanvc <pieterjanvc@hotmail.com>
2025-12-30 17:33:29 -0500

    Start setup module

---
+++ R/shiny_mod_setup.R
@@ -0,0 +1,57 @@
+#' Module UI setup tab
+#'
+#' @param id Module ID
+#'
+#' @import shiny, DT
+#'
+#' @returns Shiny UI element
+#' @export
+#'
+mod_setup_ui <- function(id) {
+  ns <- NS(id)
+  tagList(DTOutput(ns("evalsTable")))
+}
+
+#' Module server setup tab
+#'
+#' @param id Module ID
+#' @param conn connection to the database
+#'
+#' @import shiny dplyr
+#'
+#' @returns trigger if there is an update
+#'
+#' @export
+#'
+mod_setup_server <- function(id, conn) {
+  evalOverview <- function(conn) {
+    tbl(conn, "evaluation") |> collect()
+  }
+
+  moduleServer(id, function(input, output, session) {
+    output$evalsTable <- renderDT(
+      {
+        tbl(conn, "evaluation") |> collect()
+      },
+      server = T
+    )
+    return()
+  })
+}
+
+library(shiny)
+
+ui <- fluidPage(
+  mod_setup_ui("dev")
+)
+
+server <- function(input, output, session) {
+  dbInfo <- "../local/cfme.db"
+  conn <- dbGetConn(dbInfo, session = session)
+  x <- mod_setup_server("dev", conn)
+  observe({
+    print(x)
+  })
+}
+
+shinyApp(ui, server)
